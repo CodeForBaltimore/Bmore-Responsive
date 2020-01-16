89a2c693da0b7e491fd9ce9653768f856ad638b8
Jason Anton <rev.jt.anton@gmail.com>
2020-01-16 09:20:19 -0500

    adding back it tests; all pass if ERASE_DATABASE is not present in .env

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,64 +1,64 @@
 import chai from 'chai';
-// import request from 'supertest';
-// import app from '.';
+import request from 'supertest';
+import app from '.';
 import utils from './utils';
 
 const {expect} = chai;
 
-// describe('API Integration Tests', () => {
-	// it('should return uptime', done => {
-	// 	request(app)
-	// 		.get('/_healthcheck')
-	// 		.end((err, res) => {
-	// 			if (err) {
-	// 				console.error(`IT uptime error: ${err}`);
-	// 			}
+describe('API Integration Tests', () => {
+	it('should return uptime', done => {
+		request(app)
+			.get('/_healthcheck')
+			.end((err, res) => {
+				if (err) {
+					console.error(`IT uptime error: ${err}`);
+				}
 
-	// 			expect(res.body.uptime).to.be.ok;
-	// 			expect(res.statusCode).to.equal(200);
-	// 			done();
-	// 		});
-	// });
-	// it('should return environment', done => {
-	// 	request(app)
-	// 		.get('/_healthcheck')
-	// 		.end((err, res) => {
-	// 			if (err) {
-	// 				console.error(`IT environment error: ${err}`);
-	// 			}
+				expect(res.body.uptime).to.be.ok;
+				expect(res.statusCode).to.equal(200);
+				done();
+			});
+	});
+	it('should return environment', done => {
+		request(app)
+			.get('/_healthcheck')
+			.end((err, res) => {
+				if (err) {
+					console.error(`IT environment error: ${err}`);
+				}
 
-	// 			expect(res.body.environment).to.be.ok;
-	// 			expect(res.statusCode).to.equal(200);
-	// 			done();
-	// 		});
-	// });
-	// it('should return version number', done => {
-	// 	request(app)
-	// 		.get('/_healthcheck')
-	// 		.end((err, res) => {
-	// 			if (err) {
-	// 				console.error(`IT version error: ${err}`);
-	// 			}
+				expect(res.body.environment).to.be.ok;
+				expect(res.statusCode).to.equal(200);
+				done();
+			});
+	});
+	it('should return version number', done => {
+		request(app)
+			.get('/_healthcheck')
+			.end((err, res) => {
+				if (err) {
+					console.error(`IT version error: ${err}`);
+				}
 
-	// 			expect(res.body.version).to.be.ok;
-	// 			expect(res.statusCode).to.equal(200);
-	// 			done();
-	// 		});
-	// });
-	// it('should return request id', done => {
-	// 	request(app)
-	// 		.get('/_healthcheck')
-	// 		.end((err, res) => {
-	// 			if (err) {
-	// 				console.error(`IT version error: ${err}`);
-	// 			}
+				expect(res.body.version).to.be.ok;
+				expect(res.statusCode).to.equal(200);
+				done();
+			});
+	});
+	it('should return request id', done => {
+		request(app)
+			.get('/_healthcheck')
+			.end((err, res) => {
+				if (err) {
+					console.error(`IT version error: ${err}`);
+				}
 
-	// 			expect(res.body.requestId).to.be.ok;
-	// 			expect(res.statusCode).to.equal(200);
-	// 			done();
-	// 		});
-	// });
-// });
+				expect(res.body.requestId).to.be.ok;
+				expect(res.statusCode).to.equal(200);
+				done();
+			});
+	});
+});
 
 describe('Utils Tests', () => {
     it('formatTime', done => {
