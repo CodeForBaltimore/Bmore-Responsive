7f16a94f49e8fbb9148db27cc26e44eaaff7b820
Jason Anton <rev.jt.anton@gmail.com>
2020-01-31 13:05:37 -0500

    adding commented out failing model tests

---
+++ src/tests/models.spec.js
@@ -0,0 +1,23 @@
+// import chai from 'chai';
+// import request from 'supertest';
+// import app from '..';
+
+// const {expect} = chai;
+
+// describe('Model Tests', () => {
+// 	it('should create a user', done => {
+//         const user = {
+//             username: 'test', 
+//             password: 'password'
+//         }
+//         request(app)
+//             .post('/user')
+//             .set('Accept', 'application/json')
+//             .send(user)
+//             .end((err, res) => {
+//                 expect(res.status).to.equal(200);
+
+//                 done();
+//             })
+// 	});
+// });
