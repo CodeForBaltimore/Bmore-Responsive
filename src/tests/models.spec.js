d9233517bdd68242a81544908c0363ac33ae050c
Jason Anton <rev.jt.anton@gmail.com>
2020-01-31 13:50:31 -0500

    adding randomWords, fixing model test case

--- src/tests/models.spec.js
+++ src/tests/models.spec.js
@@ -1,23 +1,21 @@
 // import chai from 'chai';
-// import request from 'supertest';
-// import app from '..';
+import request from 'supertest';
+import randomWords from 'random-words';
+import app from '..';
 
 // const {expect} = chai;
 
-// describe('Model Tests', () => {
-// 	it('should create a user', done => {
-//         const user = {
-//             username: 'test', 
-//             password: 'password'
-//         }
-//         request(app)
-//             .post('/user')
-//             .set('Accept', 'application/json')
-//             .send(user)
-//             .end((err, res) => {
-//                 expect(res.status).to.equal(200);
-
-//                 done();
-//             })
-// 	});
-// });
+describe('Model Tests', () => {
+    it('should create a user', function(done) {
+        request(app)
+          .post('/user')
+          .send({username: randomWords(),password: randomWords()})
+          .set('Accept', 'application/json')
+          .expect('Content-Type', 'text/html; charset=utf-8')
+          .expect(200)
+          .end(function(err, res) {
+            if (err) return done(err);
+            done();
+          });
+      });
+});
