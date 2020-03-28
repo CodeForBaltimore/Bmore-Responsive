b235f7511f1e9169085a96bf3cef37195ad6c3f8
Gabriel Boorse <gnboorse@gmail.com>
2020-03-28 13:26:17 -0400

    add requireTLS

--- src/email/index.js
+++ src/email/index.js
@@ -4,7 +4,7 @@ import nunjucks from "nunjucks";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: true,
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD
