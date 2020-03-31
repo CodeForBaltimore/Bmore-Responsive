9c01b55699031b8801fd74b6badcb260353f91fb
Jason Anton <rev.jt.anton@gmail.com>
2020-03-31 13:52:15 -0400

    Adding all the new entities to contacts

--- sequelize/seeders/04-demo-contact.js
+++ sequelize/seeders/04-demo-contact.js
@@ -30,7 +30,23 @@ module.exports = {
 				"Kwik-E-Mart",
 				"Krusty Burger",
 				"Duff Brewery",
-				"Aztech Theater"
+				"Aztech Theater",
+				"The Copy Jalopy",
+				"99¢ Furniture Store",
+				"Little Lisa Recycling Plant",
+				"Acne Grease and Shovel",
+				"Buzz Cola",
+				"Classy Joe's",
+				"Compu-Global-Hyper-Mega-Net",
+				"Springfield Gas Company",
+				"U-Break-It Van Rental",
+				"Uncle Homer's Daycare Center",
+				"Ziffcorp",
+				"Burns Slant Drilling Co.",
+				"Springfield Gorge",
+				"Springfield Tire Yard",
+				"The Murderhorn",
+				"Nuclear Lake"
 			];
 			let name = randomWords();
 
