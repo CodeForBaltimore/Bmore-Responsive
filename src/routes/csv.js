9d719055cd97311138c622ec941813743d739d57
Sean Crim <seancrim@BLCorp-BALVCF.local>
2020-04-16 12:14:43 -0400

    updated csv route to handle empty database results and updated swagger

--- src/routes/csv.js
+++ src/routes/csv.js
@@ -14,10 +14,13 @@ router.get('/:model_type', async (req, res) => {
         if(req.context.models.hasOwnProperty(req.params.model_type)){
             const results = await req.context.models[req.params.model_type].findAll({raw:true});
 
-            const csv = results.length === 0 ? "no data found for type" : await parseAsync(JSON.parse(JSON.stringify(results)), Object.keys(results[0]), {});
-
-            code = 200;
-            message = csv;
+            if(results.length !== 0){
+                code = 200;
+                message = await parseAsync(JSON.parse(JSON.stringify(results)), Object.keys(results[0]), {});
+            } else {
+                code = 404;
+                message = "no data found for model type"
+            }
         } else {
             message = "model type is invalid"
             code = 422;
