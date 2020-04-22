93df9ab72ec572f82630419ca5f8c8123dc4b9ec
rikkcaro <rikk.caro@gmail.com>
2020-04-22 19:06:40 -0400

    Initial swag for getting the user route using JWT's every go round.

--- src/tests/user.routes.spec.js
+++ src/tests/user.routes.spec.js
@@ -3,89 +3,103 @@ import request from 'supertest';
 import randomWords from 'random-words';
 import app from '..';
 
-const { expect } = chai;
+const { describe, before} = chai;
+const { expect } = chakram; // use the chakram expectations
+const { serviceUrl, login } = require('../data/config');
+
 const user = { email: `${randomWords()}@test.test`, password: randomWords(), roles: [1] };
+const chakram = require('chakram');
+
+
+
+
 
 /** @todo add JWT validation tests */
 
-describe('User positive tests', () => {
-  it('should create a user', (done) => {
-    request(app)
-      .post('/user')
-      .send(user)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal(`${user.email} created`);
-        done();
-      });
-  });
-  it('should login a user', (done) => {
-    request(app)
-      .post('/user/login')
-      .send(user)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        done();
-      });
-  });
-  it('should get all users', (done) => {
-    request(app)
-      .get(`/user`)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.body._meta.total).to.be.greaterThan(0);
-        done();
-      });
-  });
-  it('should get a single user', (done) => {
-    request(app)
-      .get(`/user/${user.email}`)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.body.email).to.equal(user.email);
-        done();
-      });
-  });
-  it('should update a user', (done) => {
-    user.password = randomWords();
-    request(app)
-      .put('/user')
-      .send(user)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal(`${user.email} updated`);
-        done();
-      });
-  });
-  it('should delete a user', (done) => {
-    request(app)
-      .delete(`/user/${user.email}`)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal(`${user.email} deleted`);
-        done();
-      });
-  });
-});
-
+{
+    module.exports = () => {
+        let tokenHeader;
+        before(async () => {
+            tokenHeader = { Authorization: await login() };
+        });
+    describe('User positive tests', () => {
+        it('should create a user', (done) => {
+            request(app)
+                .post('/user')
+                .send(user)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'text/html; charset=utf-8')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    expect(res.text).to.equal(`${user.email} created`);
+                    done();
+                });
+        });
+        it('should login a user', (done) => {
+            request(app)
+                .post('/user/login')
+                .send(user)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'text/html; charset=utf-8')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
+        it('should get all users', (done) => {
+            request(app)
+                .get(`/user`)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    expect(res.body._meta.total).to.be.greaterThan(0);
+                    done();
+                });
+        });
+        it('should get a single user', (done) => {
+            request(app)
+                .get(`/user/${user.email}`)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    expect(res.body.email).to.equal(user.email);
+                    done();
+                });
+        });
+        it('should update a user', (done) => {
+            user.password = randomWords();
+            request(app)
+                .put('/user')
+                .send(user)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'text/html; charset=utf-8')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    expect(res.text).to.equal(`${user.email} updated`);
+                    done();
+                });
+        });
+        it('should delete a user', (done) => {
+            request(app)
+                .delete(`/user/${user.email}`)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'text/html; charset=utf-8')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    expect(res.text).to.equal(`${user.email} deleted`);
+                    done();
+                });
+        });
+    });
+}
 describe('User negative tests', () => {
   it('should not login a user', (done) => {
     request(app)
