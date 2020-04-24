ac338d98e461415ba27f9a86194a7c0d13b2c8b1
rikkcaro <rikk.caro@gmail.com>
2020-04-24 09:53:17 -0400

    Fixed the structure for the nested function.

--- src/tests/user.routes.spec.js
+++ src/tests/user.routes.spec.js
@@ -20,150 +20,151 @@ const chakram = require('chakram');
     module.exports = () => {
         let tokenHeader;
         before(async () => {
-            tokenHeader = { Authorization: await login() };
+            tokenHeader = {Authorization: await login()};
         });
-    describe('User positive tests', () => {
-        it('should create a user', (done) => {
+        describe('User positive tests', () => {
+            it('should create a user', (done) => {
+                request(app)
+                    .post('/user')
+                    .send(user)
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', 'text/html; charset=utf-8')
+                    .expect(200)
+                    .end((err, res) => {
+                        if (err) return done(err);
+                        expect(res.text).to.equal(`${user.email} created`);
+                        done();
+                    });
+            });
+            it('should login a user', (done) => {
+                request(app)
+                    .post('/user/login')
+                    .send(user)
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', 'text/html; charset=utf-8')
+                    .expect(200)
+                    .end((err, res) => {
+                        if (err) return done(err);
+                        done();
+                    });
+            });
+            it('should get all users', (done) => {
+                request(app)
+                    .get(`/user`)
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', 'application/json; charset=utf-8')
+                    .expect(200)
+                    .end((err, res) => {
+                        if (err) return done(err);
+                        expect(res.body._meta.total).to.be.greaterThan(0);
+                        done();
+                    });
+            });
+            it('should get a single user', (done) => {
+                request(app)
+                    .get(`/user/${user.email}`)
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', 'application/json; charset=utf-8')
+                    .expect(200)
+                    .end((err, res) => {
+                        if (err) return done(err);
+                        expect(res.body.email).to.equal(user.email);
+                        done();
+                    });
+            });
+            it('should update a user', (done) => {
+                user.password = randomWords();
+                request(app)
+                    .put('/user')
+                    .send(user)
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', 'text/html; charset=utf-8')
+                    .expect(200)
+                    .end((err, res) => {
+                        if (err) return done(err);
+                        expect(res.text).to.equal(`${user.email} updated`);
+                        done();
+                    });
+            });
+            it('should delete a user', (done) => {
+                request(app)
+                    .delete(`/user/${user.email}`)
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', 'text/html; charset=utf-8')
+                    .expect(200)
+                    .end((err, res) => {
+                        if (err) return done(err);
+                        expect(res.text).to.equal(`${user.email} deleted`);
+                        done();
+                    });
+            });
+        });
+    }
+    describe('User negative tests', () => {
+        it('should not login a user', (done) => {
+            request(app)
+                .post('/user/login')
+                .send({email: randomWords(), password: randomWords()})
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'text/html; charset=utf-8')
+                .expect(422)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    expect(res.text).to.equal('Invalid input')
+                    done();
+                });
+        });
+        it('should not create a user', (done) => {
+            user.email = randomWords();
             request(app)
                 .post('/user')
                 .send(user)
                 .set('Accept', 'application/json')
                 .expect('Content-Type', 'text/html; charset=utf-8')
-                .expect(200)
+                .expect(422)
                 .end((err, res) => {
                     if (err) return done(err);
-                    expect(res.text).to.equal(`${user.email} created`);
+                    expect(res.text).to.equal('Invalid input')
                     done();
                 });
         });
-        it('should login a user', (done) => {
-            request(app)
-                .post('/user/login')
-                .send(user)
-                .set('Accept', 'application/json')
-                .expect('Content-Type', 'text/html; charset=utf-8')
-                .expect(200)
-                .end((err, res) => {
-                    if (err) return done(err);
-                    done();
-                });
-        });
-        it('should get all users', (done) => {
-            request(app)
-                .get(`/user`)
-                .set('Accept', 'application/json')
-                .expect('Content-Type', 'application/json; charset=utf-8')
-                .expect(200)
-                .end((err, res) => {
-                    if (err) return done(err);
-                    expect(res.body._meta.total).to.be.greaterThan(0);
-                    done();
-                });
-        });
-        it('should get a single user', (done) => {
+        it('should not get a single user', (done) => {
             request(app)
                 .get(`/user/${user.email}`)
                 .set('Accept', 'application/json')
-                .expect('Content-Type', 'application/json; charset=utf-8')
-                .expect(200)
+                .expect(422)
                 .end((err, res) => {
                     if (err) return done(err);
-                    expect(res.body.email).to.equal(user.email);
+                    expect(res.body.email).to.be.an('undefined');
                     done();
                 });
         });
-        it('should update a user', (done) => {
+        it('should not update a user', (done) => {
             user.password = randomWords();
             request(app)
                 .put('/user')
                 .send(user)
                 .set('Accept', 'application/json')
                 .expect('Content-Type', 'text/html; charset=utf-8')
-                .expect(200)
+                .expect(422)
                 .end((err, res) => {
                     if (err) return done(err);
-                    expect(res.text).to.equal(`${user.email} updated`);
+                    expect(res.text).to.equal(`Invalid input`);
                     done();
                 });
         });
-        it('should delete a user', (done) => {
+        it('should not delete a user', (done) => {
+            user.password = randomWords();
             request(app)
-                .delete(`/user/${user.email}`)
+                .delete(`/user/${user.username}`)
                 .set('Accept', 'application/json')
                 .expect('Content-Type', 'text/html; charset=utf-8')
-                .expect(200)
+                .expect(422)
                 .end((err, res) => {
                     if (err) return done(err);
-                    expect(res.text).to.equal(`${user.email} deleted`);
+                    expect(res.text).to.equal(`Invalid input`);
                     done();
                 });
         });
     });
 }
-describe('User negative tests', () => {
-  it('should not login a user', (done) => {
-    request(app)
-      .post('/user/login')
-      .send({ email: randomWords(), password: randomWords() })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(422)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal('Invalid input')
-        done();
-      });
-  });
-  it('should not create a user', (done) => {
-    user.email = randomWords();
-    request(app)
-      .post('/user')
-      .send(user)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(422)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal('Invalid input')
-        done();
-      });
-  });
-  it('should not get a single user', (done) => {
-    request(app)
-      .get(`/user/${user.email}`)
-      .set('Accept', 'application/json')
-      .expect(422)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.body.email).to.be.an('undefined');
-        done();
-      });
-  });
-  it('should not update a user', (done) => {
-    user.password = randomWords();
-    request(app)
-      .put('/user')
-      .send(user)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(422)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal(`Invalid input`);
-        done();
-      });
-  });
-  it('should not delete a user', (done) => {
-    user.password = randomWords();
-    request(app)
-      .delete(`/user/${user.username}`)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(422)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal(`Invalid input`);
-        done();
-      });
-  });
-});
