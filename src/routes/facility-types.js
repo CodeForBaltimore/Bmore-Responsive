e231c39d7c7785b1ac0b7efbf05bdaa61c08e60c
Orbelin Palacios <opalacios@bellese.io>
2020-12-02 10:51:21 -0500

    using faker library to generate dummy data.

--- src/routes/facility-types.js
+++ src/routes/facility-types.js
@@ -1,15 +1,12 @@
 import {Router} from 'express'
 import utils from '../utils'
+import {FACILITY_TYPES} from '../constants/facilities.const'
 
 const router = new Router()
 router.use(utils.authMiddleware)
 
 router.get('/', async (req, res) => {
-    return res.status(200).send([
-        "Assisted Living Facility", 
-        "Mixed Housing", 
-        "Senior Housing",
-    ])
+    return res.status(200).send(FACILITY_TYPES)
 })
 
 export default router
