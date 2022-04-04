2ae89ecf2686806f816249f6bee94e67a7a1a625
nmarklund10 <nmarklund10@outlook.com>
2022-04-03 21:18:58 -0400

    add health v2 endpoint functionality

---
+++ src/routes/v2/health.js
@@ -0,0 +1,37 @@
+import { Router } from 'express'
+import utils from '../../utils'
+import { sequelize } from '../../models'
+
+const router = new Router()
+
+const checkDatabase = async (req, res) => {
+  const databaseCheckResult = {
+    "name": "database",
+    "status": "healthy",
+    "message": "N/A"
+  }
+  try {
+    // https://sequelize.org/docs/v6/getting-started/#testing-the-connection
+    await sequelize.authenticate()
+  }
+  catch (error) {
+    console.error(`Database seems to be down: '${error}'`)
+    databaseCheckResult.status = "down"
+    databaseCheckResult.message = error.message
+  }
+  return databaseCheckResult
+}
+
+router.get('/', async (req, res) => {
+  res.status(200).json({
+    uptime: utils.formatTime(process.uptime()),
+    environment: process.env.NODE_ENV || 'n/a',
+    version: process.env.npm_package_version || 'n/a',
+    requestId: req.id,
+    checks: [
+      await checkDatabase()
+    ]
+  })
+})
+
+export default router
---
+++ src/routes/v2/index.js
@@ -0,0 +1,8 @@
+import health from './health'
+// Exports object of routes we import above. Add to this if you're adding new routes.
+
+const routeExports = {
+  health
+}
+
+module.exports = routeExports
