d4b1a6a2473c513512a1affa5b40e92d4a2eb242
Your Name <you@example.com>
2022-05-20 12:57:03 -0400

    export explicit functions from utils/v1

--- src/tests/v1/utils.spec.js
+++ src/tests/v1/utils.spec.js
@@ -1,20 +1,19 @@
 import chai from 'chai'
-import utils from '../../utils'
 import { Login } from '../../utils/login'
-import { Response } from '../../utils/v1'
+import { Response, formatTime, validatePassword } from '../../utils/v1'
 
 const VERSION = '1'
 const { assert, expect } = chai
 
 describe(`Utils Tests (v${VERSION})`, function () {
   it('should return 00:00:01', function (done) {
-    expect(utils.formatTime(1)).to.equal('00:00:01')
+    expect(formatTime(1)).to.equal('00:00:01')
     done()
   })
   it('should validate passwords', function (done) {
-    const valid = utils.validatePassword('Abcdef123$')
+    const valid = validatePassword('Abcdef123$')
     expect(valid).to.be.true
-    const invalid = utils.validatePassword('abc123')
+    const invalid = validatePassword('abc123')
     expect(invalid).to.be.false
     done()
   })
@@ -25,18 +24,18 @@ describe(`Utils Tests (v${VERSION})`, function () {
       done()
     })
     it('should be an instance of Response', function (done) {
-      expect(new utils.Response()).to.be.an.instanceof(Response)
+      expect(new Response()).to.be.an.instanceof(Response)
       done()
     })
     it('should get all codes', function (done) {
-      let res = new utils.Response()
+      let res = new Response()
       let codes = res.getCodes()
       assert.typeOf(codes, 'object', 'it is an object')
       expect(codes[200]).to.equal('OK')
       done()
     })
     it('should set a default message', function (done) {
-      let res = new utils.Response()
+      let res = new Response()
       res.setCode(500)
       res.setMessage()
 
