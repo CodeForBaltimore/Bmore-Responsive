7cb9a023d586a1ba0be939cbe7f11a9ed4d07ae4
Your Name <you@example.com>
2022-05-20 15:12:20 -0400

    user ErrorResponse

--- src/routes/v2/security.js
+++ src/routes/v2/security.js
@@ -1,32 +1,33 @@
 import { Router } from 'express'
 import validator from 'validator'
-import { Response } from '../../utils/v2'
+import { ErrorResponse } from '../../utils/v2'
 
 const router = new Router()
 
 router.get('/authenticate', async (req, res) => {
-  const response = new Response()
+  let response
   try {
     const { email, password } = req.body
     if (validator.isEmail(email)) {
       const token = await req.context.models.User.findByLogin(email.toLowerCase(), password)
       if (token) {
         console.log(token)
-        response.setMessage({
-          'token': token,
+        return res.status(200).json({
+          'token': token
         })
       } else {
-        response.setCode(403)
+        response = new ErrorResponse(401)
       }
     } else {
-      response.setCode(400)
+      response = new ErrorResponse(400)
+      response.addDetail('email', 'Address provided is invalid')
     }
   } catch (e) {
     console.error(e)
-    response.setCode(500)
+    response = new ErrorResponse(500)
   }
 
-  return res.status(response.getCode()).json(response.getMessage())
+  return res.status(response.getCode()).json(response.getBody())
 })
 
 export default router
