c63dcd5b79fa2b7e6d29403042f6833e28f82ee3
Your Name <you@example.com>
2022-05-20 13:28:49 -0400

    remove unused import

--- src/tests/v2/user.routes.spec.js
+++ src/tests/v2/user.routes.spec.js
@@ -1,7 +1,6 @@
 import chai from 'chai'
 import request from 'supertest'
 import randomWords from 'random-words'
-import { Login } from '../../utils/login'
 import app from '../..'
 
 const VERSION = '2'
